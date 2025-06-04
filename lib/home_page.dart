5a74f569e0b97fd74efe12970513d7eb98184c75
suhyunChoo <s_where@naver.com>
2025-06-04 14:16:28 +0900

    feat: appbar 추가

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,6 +4,10 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Diary'), // 앱바 제목
+        ),
+    );
   }
 }
